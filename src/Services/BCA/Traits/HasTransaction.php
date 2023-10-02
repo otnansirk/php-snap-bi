f3d1ce2a71780c88c27c056d33bdfa0f2ea14663
k15 <krisnantobiyuh@gmail.com>
2023-10-02 08:04:13 +0700

    feat: :sparkles: bca direct debit payment

--- src/Services/BCA/Traits/HasTransaction.php
+++ src/Services/BCA/Traits/HasTransaction.php
@@ -48,4 +48,72 @@ trait HasTransaction
             ->withHeaders($headers)
             ->post($url, $body);
     }
+
+    /**
+     * This service is used to pre-processing OneKlik Registration by Generating OneKlik Registration Web Token.
+     *
+     * @return HttpResponseInterface
+     */
+    public static function directDebitPayment(): HttpResponseInterface
+    {
+        // Required access token
+        self::authenticated();
+        $path = "/openapi/oneklik/v1.0/debit/payment-host-to-host";
+
+        $timestamp   = currentTimestamp()->toIso8601String();
+        $accessToken = self::$token;
+
+        $body = array_merge([
+            "partnerReferenceNo" => Uuid::uuid4(),
+            "accountNo"          => BcaConfig::get('account_id'),
+            "merchantId"         => BcaConfig::get('partner_id'),
+            "bankCardToken"      => BcaConfig::get('bank_card_token')
+        ], self::$body);
+
+        $headers = array_merge([
+            'X-TIMESTAMP'   => $timestamp,
+            'X-SIGNATURE'   => Signature::symmetric(BcaConfig::class, 'POST', $path, $body, $timestamp, $accessToken),
+            'CHANNEL-ID'    => BcaConfig::get('channel_id'),
+            'X-PARTNER-ID'  => BcaConfig::get('partner_id'),
+            'X-EXTERNAL-ID' => int_rand(16),
+        ], self::$headers);
+
+        $url = BcaConfig::get('base_url') . $path;
+        return Http::withToken($accessToken)
+            ->withHeaders($headers)
+            ->post($url, $body);
+    }
+
+    /**
+     * This service is used to inquiry OneKlik transaction status.
+     *
+     * @return HttpResponseInterface
+     */
+    public static function directDebitPaymentStatus(): HttpResponseInterface
+    {
+        // Required access token
+        self::authenticated();
+        $path = "/openapi/oneklik/v1.0/debit/status";
+
+        $timestamp   = currentTimestamp()->toIso8601String();
+        $accessToken = self::$token;
+
+        $body = array_merge([
+            "partnerReferenceNo" => Uuid::uuid4(),
+            "serviceCode"        => '54',
+        ], self::$body);
+
+        $headers = array_merge([
+            'X-TIMESTAMP'   => $timestamp,
+            'X-SIGNATURE'   => Signature::symmetric(BcaConfig::class, 'POST', $path, $body, $timestamp, $accessToken),
+            'CHANNEL-ID'    => BcaConfig::get('channel_id'),
+            'X-PARTNER-ID'  => BcaConfig::get('partner_id'),
+            'X-EXTERNAL-ID' => int_rand(16),
+        ], self::$headers);
+
+        $url = BcaConfig::get('base_url') . $path;
+        return Http::withToken($accessToken)
+            ->withHeaders($headers)
+            ->post($url, $body);
+    }
 }
