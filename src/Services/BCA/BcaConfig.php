0d419175e229aa060ba5ad201cb4e89d5883353a
k15 <krisnantobiyuh@gmail.com>
2023-10-02 08:29:30 +0700

    feat: :sparkles: register default header & body to bca config

--- src/Services/BCA/BcaConfig.php
+++ src/Services/BCA/BcaConfig.php
@@ -3,11 +3,42 @@
 namespace Otnansirk\SnapBI\Services\BCA;
 
 use Otnansirk\SnapBI\Interfaces\ConfigInterface;
+use Otnansirk\SnapBI\Support\Signature;
 use Otnansirk\SnapBI\Traits\HasConfig;
 use Otnansirk\SnapBI\Traits\HasSelfCall;
+use Ramsey\Uuid\Uuid;
 
 
 final class BcaConfig implements ConfigInterface
 {
     use HasConfig, HasSelfCall;
+
+    /**
+     * Default headers
+     *
+     * @return array
+     */
+    public static function defaultHeaders(): array
+    {
+
+        return [
+            'CHANNEL-ID'    => BcaConfig::get('channel_id'),
+            'X-PARTNER-ID'  => BcaConfig::get('partner_id'),
+            'X-EXTERNAL-ID' => int_rand(16),
+        ];
+    }
+
+    /**
+     * Default body
+     *
+     * @return array
+     */
+    public static function defaultBody(): array
+    {
+        return [
+            "partnerReferenceNo" => Uuid::uuid4(),
+            "accountNo"          => BcaConfig::get('account_id'),
+            "bankCardToken"      => BcaConfig::get('bank_card_token')
+        ];
+    }
 }
